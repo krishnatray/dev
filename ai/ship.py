d70581de8830782808587894bc3800b4233cb91f
Sushil Sharma <ssharma@carbon3d.com>
2021-02-16 23:34:40 +0530

    added aliens

--- ai/ship.py
+++ ai/ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         self.image = pygame.image.load('ship.bmp')
@@ -12,6 +13,23 @@ class Ship:
 
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Store a decimal value for ship's x position
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """ Update the ship's position """
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rectangle object from self.x   
+        self.rect.x = self.x
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
